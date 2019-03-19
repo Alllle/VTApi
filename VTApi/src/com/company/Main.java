1fd40c90fd8c41d91b990bf76a4dfee8a800916e
Arvid <algotify@gmail.com>
2019-03-19 22:05:13 +0100

    testing stuff

--- VTApi/src/com/company/Main.java
+++ VTApi/src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Base64;
 import java.util.HashMap;
@@ -10,7 +12,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-	    APIRequest api = new APIRequest();
         Map<String,String> arguments = new HashMap<>();
 	    main.sendPost();
     }
@@ -34,16 +35,24 @@ public class Main {
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "https://api.vasttrafik.se:443/token");
         con.setRequestProperty("Authorization", Base64.getEncoder().encodeToString((key + ":" + secret).getBytes()));
-        con.setRequestProperty("grant_type", "4888239");
+        //con.setRequestProperty("grant_type", "4888239");
         //con.setRequestProperty("format", "json");
 
         con.setDoOutput(true);
 
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(arguments);
+        wr.writeBytes("grant_type=client_credentials&scope=<543543543>");
         wr.flush();
         wr.close();
         int responsCode = con.getResponseCode();
         System.out.println("respons code: " + responsCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        StringBuilder body = new StringBuilder();
+        String inputLine;
+        while((inputLine = in.readLine()) != null) {
+            body.append(inputLine);
+        }
+        System.out.println(inputLine);
     }
 }
