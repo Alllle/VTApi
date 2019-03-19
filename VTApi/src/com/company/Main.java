ec433b0a97edd6eb0c97f7a6b404728d325a0249
Arvid <algotify@gmail.com>
2019-03-19 23:30:23 +0100

    Fix POST requests :)

--- VTApi/src/com/company/Main.java
+++ VTApi/src/com/company/Main.java
@@ -21,38 +21,28 @@ public class Main {
         String secret = "EjmHVMooqxkH3A8eecSXkF2onhga";
         String url = "https://api.vasttrafik.se/token";
 
-        String arguments = "Content-Type: https://api.vasttrafik.se:443/token\nAuthorization: Basic " + Base64.getEncoder().encodeToString((key + ":" + secret).getBytes());
-        //arguments.put("grant_type", "uniqueID");
-        StringBuilder stringArgument = new StringBuilder();
-
-
-        System.out.println(arguments);
-
-
         URL obj = new URL(url);
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
         con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "https://api.vasttrafik.se:443/token");
-        con.setRequestProperty("Authorization", Base64.getEncoder().encodeToString((key + ":" + secret).getBytes()));
-        //con.setRequestProperty("grant_type", "4888239");
-        //con.setRequestProperty("format", "json");
+        con.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString((key + ":" + secret).getBytes()));
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
         con.setDoOutput(true);
 
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes("grant_type=client_credentials&scope=<543543543>");
+        wr.writeBytes("grant_type=client_credentials&scope=4566");
         wr.flush();
         wr.close();
         int responsCode = con.getResponseCode();
         System.out.println("respons code: " + responsCode);
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
         StringBuilder body = new StringBuilder();
         String inputLine;
         while((inputLine = in.readLine()) != null) {
             body.append(inputLine);
         }
-        System.out.println(inputLine);
+        System.out.println(body.toString());
     }
 }
